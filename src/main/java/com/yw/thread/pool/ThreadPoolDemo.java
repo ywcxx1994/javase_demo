b3ed3f1175ffe30f6be7ca3fcecd72e3cf5fd556
yanwei <yanwei@wxdata.cn>
2019-11-30 14:11:56 +0800

    自定义ThreadFactry

--- src/main/java/com/yw/thread/pool/ThreadPoolDemo.java
+++ src/main/java/com/yw/thread/pool/ThreadPoolDemo.java
@@ -1,10 +1,8 @@
 package com.yw.thread.pool;
 
 
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.RejectedExecutionHandler;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author:YanWei
@@ -12,22 +10,24 @@ import java.util.concurrent.TimeUnit;
  * @desc: 线程池测试
  */
 public class ThreadPoolDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        CountDownLatch countDownLatch = new CountDownLatch(15);
+        ThreadFactory threadFactory = new MyThreadFactory();
         ThreadPoolExecutor threadPoolExecutor
-                = new ThreadPoolExecutor(5, 10, 200,
-                TimeUnit.MILLISECONDS, new ArrayBlockingQueue<Runnable>(5),new MyRejectHandler());
+            = new ThreadPoolExecutor(5, 10, 200,
+            TimeUnit.MILLISECONDS, new ArrayBlockingQueue<Runnable>(5),new MyRejectHandler());
         //当把i设置为20时，会出发ThreadPoolExecutor的拒绝策略。
         for (int i = 0; i < 20; i++) {
 
-            Task task = new Task(i,threadPoolExecutor);
+            Task task = new Task(i,threadPoolExecutor,countDownLatch);
             threadPoolExecutor.execute(task);
-//            threadPoolExecutor.submit(()->{
-//
-//            });
             System.out.println("线程池中线程数目：" + threadPoolExecutor.getPoolSize() +
                     "，队列中等待执行的任务数目：" + threadPoolExecutor.getQueue().size() + "，已执行的任务数目：" + threadPoolExecutor.getCompletedTaskCount());
         }
-        threadPoolExecutor.shutdown();
+        countDownLatch.await();
+        System.out.println("active:"+threadPoolExecutor.getPoolSize());
+        //若不关闭线程池，则始终会有5个活跃线程
+//        threadPoolExecutor.shutdown();
     }
 
 }
@@ -36,20 +36,23 @@ class Task implements Runnable {
 
     int taskNum;
     ThreadPoolExecutor threadPoolExecutor;
-    public Task(int taskNum,ThreadPoolExecutor threadPoolExecutor) {
+    CountDownLatch countDownLatch;
+    public Task(int taskNum,ThreadPoolExecutor threadPoolExecutor,CountDownLatch countDownLatch) {
         this.taskNum = taskNum;
         this.threadPoolExecutor = threadPoolExecutor;
+        this.countDownLatch = countDownLatch;
     }
 
     @Override
     public void run() {
-        System.out.println("正在执行：" + taskNum);
+        System.out.println(Thread.currentThread().getName()+"正在执行：" + taskNum);
         try {
             Thread.sleep(4000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.out.println("执行完毕：" + taskNum+"线程数目：" + threadPoolExecutor.getPoolSize() +
+        countDownLatch.countDown();
+        System.out.println(Thread.currentThread().getName()+"执行完毕：" + taskNum+"线程数目：" + threadPoolExecutor.getPoolSize() +
                 "，等待执行的任务数目：" + threadPoolExecutor.getQueue().size() + "，已执行的任务数目：" + threadPoolExecutor.getCompletedTaskCount());
     }
 }
@@ -60,5 +63,14 @@ class MyRejectHandler implements RejectedExecutionHandler{
             Task task = (Task)r;
             System.out.println("拒绝线程任务："+task.taskNum+"任务已超最大限度，不在接受");
         }
+    }
+}
+class MyThreadFactory implements ThreadFactory{
+    @Override
+    public Thread newThread(Runnable r) {
+        AtomicInteger count = new AtomicInteger(0);
+        String prefix = "yw-pool";
+        Thread thread = new Thread(prefix+count.getAndIncrement());
+        return thread;
     }
 }
